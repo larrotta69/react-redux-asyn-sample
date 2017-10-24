6b2a8df186943051e24667dc128812826ceece04
dlarrota <dlarrota@hugeinc.com>
2017-10-24 18:18:54 -0500

    Optimizing Layout - no connection to Redux

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
 
-import { Route} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 
-import {errorReset} from './../../containers/Error/ErrorFeatures'
-
 const Layout = ({component: Component, ...rest}) => {
+    const handlerRender = (matchProps) => {
+        return (<div>
+            <Header />
+            <Component {...matchProps} />
+            <Footer />
+        </div>)
+    }
     return (
-        <Route {...rest} render={matchProps => (
-            <div>
-                <Header />
-                <Component {...matchProps} />
-                <Footer />
-            </div>
-        )} />
+        <Route {...rest} render={handlerRender} />
     )
 }
 /*
@@ -27,10 +25,4 @@ Layout.propTypes = {
     component: PropTypes.func.isRequired
 }
 
-export default connect(
-    (state) => ({
-        isError: state.reducerError.isError,
-        isLoaderOn: state.reducerLoader.isLoaderOn
-    }),
-    {errorReset}
-)(Layout)
+export default Layout
