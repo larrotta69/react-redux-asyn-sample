fe1741ccd64b6aed6ebd803652f7d5d07f5a6815
dlarrota <dlarrota@hugeinc.com>
2017-10-30 13:16:24 -0500

    removing the console.log

--- src/pages/Page/Page.js
+++ src/pages/Page/Page.js
@@ -36,7 +36,6 @@ class App extends React.Component {
     render() {
         const {isLoading, isError} = this.state
         const {errorMsg} = this.props
-        console.log('da')
         return (
             <StyledApp>
                 {this.props.children}
